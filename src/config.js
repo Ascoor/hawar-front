6dd44f9a197e2312ee05549fed4b2952d16bca06
askar <mr.askar1984@gmail.com>
2023-07-17 08:31:35 +0300

    api

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const API_CONFIG = {
     // baseURL  الخاص بك وفقًا لعنوان  API  قم بتعديل قيمة الـ 
-     //    baseURL: 'https://ask-ar.com/hawar-back-end', /// Server Avocat API
-  baseURL: 'http://localhost:8000', //
+        baseURL: 'https://ask-ar.com/hawar/back-end', /// Server Avocat API
+  //baseURL: 'http://localhost:8000', //
 
 };
 
