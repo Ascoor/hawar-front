f868cbb6b30b020f4c386bdd733c3d81e518e3a8
askar <mr.askar1984@gmail.com>
2023-07-23 23:34:20 +0300

    ASDAD

--- src/components/Tools/Footer.js
+++ src/components/Tools/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import './Footer.css';
+
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
@@ -16,9 +19,11 @@ const Footer = () => {
             <h5>Links</h5>
             {/* Add footer links here */}
           </Col>
-          <Col xs={12} md={12} lg={4}>
-
+          <Col xs={12} md={12} lg={4} className="center-content">
             <h6>Copyrtights {currentYear} &copy; Hawar Sport Club</h6>
+            <a href="https://www.facebook.com/hawarsportclub" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faFacebook} size="2x" className="facebook-icon" />
+            </a>
           </Col>
         </Row>
       </Container>
