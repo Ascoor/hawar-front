f32458fa9dfc94ddd7b9e230f25ce41bfe382db0
askar <mr.askar1984@gmail.com>
2023-07-30 15:34:34 +0300

    as

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const API_CONFIG = {
     // baseURL  الخاص بك وفقًا لعنوان  API  قم بتعديل قيمة الـ
-//baseURL: 'https://hawar-api.ask-ar.com', /// Server Askar API
-     baseURL: 'http://localhost:8000', //
+baseURL: 'https://hawar-api.ask-ar.com', /// Server Askar API
+   //  baseURL: 'http://localhost:8000', //
 
 };
 
